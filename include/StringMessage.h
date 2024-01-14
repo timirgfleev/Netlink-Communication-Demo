5620243cf80639cca30eef315395555d758ef895
muta <timirgfleev@gmail.com>
2024-01-15 02:19:30 +0300

    Add StringMessage class and update send.cpp

---
+++ include/StringMessage.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <netlink/msg.h>
+#include <iostream>
+#include <cstring>
+#include "Connection.h"
+
+#define NLINK_MSG_LEN 1024
+
+class StringMessage
+{
+public:
+    /*
+    * @brief Create a new message
+    * @param message The message to send
+    * @return A pointer to the message or nullptr if an error occurred
+    */
+    StringMessage(const std::string& message)
+        : msg(nlmsg_alloc())
+    {
+        if (!msg)
+        {
+            std::cerr << "Failed to allocate netlink message.\n";
+            return;
+        }
+
+        if (nlmsg_append(msg, (void *)message.c_str(), NLINK_MSG_LEN, NLMSG_ALIGNTO) < 0)
+        {
+            std::cerr << "Failed to append payload to netlink message.\n";
+            nlmsg_free(msg);
+            msg = nullptr;
+        }
+    }
+
+    ~StringMessage()
+    {
+        if (msg)
+        {
+            nlmsg_free(msg);
+        }
+    }
+    /*
+    * @brief Send the message
+    * @param sk The socket to send the message on
+    * @return True if the message was sent successfully, false otherwise
+    */
+    bool send(nl_sock *sk)
+    {
+        if (!msg)
+        {
+            return false;
+        }
+
+        nl_socket_set_peer_groups(sk, 0);
+        nl_socket_set_peer_port(sk, 2);
+
+        int msgcode = nl_send_auto(sk, msg);
+        if (msgcode < 0)
+        {
+            std::cerr << "code: " << msgcode << " Failed to send netlink message.\n";
+            return false;
+        }
+
+        std::cout << "Sent message: " << (char *)nlmsg_data(nlmsg_hdr(msg)) << "\n";
+        return true;
+    }
+
+private:
+    nl_msg *msg;
+};
--- send/send.cpp
+++ send/send.cpp
@@ -7,7 +7,7 @@
 
 
 #include <Connection.h>
-
+#include <StringMessage.h>
 int main()
 {
     // Create a new socket
@@ -20,60 +20,12 @@ int main()
     }
     nl_sock *sk = c->getSocket();
 
-    // Create a new netlink message
-    nl_msg *msg = nlmsg_alloc();
-    if (!msg)
-    {
-        std::cerr << "Failed to allocate netlink message.\n";
-        nl_socket_free(sk);
-        return -1;
-    }
-
-    // Set the PID of the message
-    // Create a sockaddr_nl structure
-    struct sockaddr_nl src_addr;
-
-    // Clear the structure
-    memset(&src_addr, 0, sizeof(src_addr));
-
-    // Set the family and pid fields
-    src_addr.nl_family = AF_NETLINK;
-    src_addr.nl_pid = nl_socket_get_local_port(sk);
-
-    // Set the source address of the message
-    nlmsg_set_src(msg, &src_addr);
-
-    // Add the message payload
-    if (nlmsg_append(msg, (void *)"Hello World !", NLINK_MSG_LEN, NLMSG_ALIGNTO) < 0)
-    {
-        std::cerr << "Failed to append payload to netlink message.\n";
-        nlmsg_free(msg);
-        return -1;
-    }
-
-    // Set the destination address of the message
+    // Set the destination address of the message and no groups
     nl_socket_set_peer_groups(sk, 0);
     nl_socket_set_peer_port(sk, 2);
 
-    // Send the message
-    // Set the sequence number of the message
-   // nlmsg_set_seq(msg, 1);
-
-    // Set the PID of the message
-    nlmsg_set_src(msg, &src_addr);
-
-    int msgcode = nl_send_auto(sk, msg);
-    if (msgcode < 0)
-    {
-        std::cerr << "code: " << msgcode << " Failed to send netlink message.\n";
-        nlmsg_free(msg);
-        return -1;
-    }
-
-    std::cout << "Sent message: Hello World !\n";
-
-    // Free the message and the socket
-    nlmsg_free(msg);
-
+    // Create a new message
+    auto message = std::make_unique<StringMessage>("desu~~~");
+    message->send(sk);
     return 0;
 }
